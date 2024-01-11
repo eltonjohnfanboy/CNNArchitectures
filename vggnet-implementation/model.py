17fd27ecbad287fc3d578ee5411d04bc77dad711
eltonjohnfanboy <adarsh0x1702@gmail.com>
2024-01-11 19:16:26 +0100

    batch norm fix

--- vggnet-implementation/model.py
+++ vggnet-implementation/model.py
@@ -9,42 +9,55 @@ class VGGnet(nn.Module):
 
         self.block1 = nn.Sequential(
             nn.Conv2d(input_dim, 64, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2)
         )
         self.block2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2)
         )
         self.block3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2)
         )
         self.block4 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Conv2d(512, 512, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Conv2d(512, 512, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2)
         )
         self.block5 = nn.Sequential(
             nn.Conv2d(512, 512, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Conv2d(512, 512, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Conv2d(512, 512, kernel_size = 3, stride = 1, padding = 'same'),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2)
         )
@@ -67,4 +80,3 @@ class VGGnet(nn.Module):
         out = out.view(-1, out.shape[1]*out.shape[2]*out.shape[3])
         out = self.classifier(out)
         return out
-    
